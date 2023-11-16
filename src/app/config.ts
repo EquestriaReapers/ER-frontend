9af53a9c25cecc0db52bd5d3a130d8ec9cae646c
Paniagua <114714690+luizzzito@users.noreply.github.com>
2023-11-15 20:43:37 -0400

    Agregar "https"

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_V1_URL = "http://localhost:3000/api/v1";
-export const BACKEND_V2_URL = "http://ucab-profile-backend.playoffside.online/api/v1";
+export const BACKEND_V2_URL = "https://ucab-profile-backend.playoffside.online/api/v1";
