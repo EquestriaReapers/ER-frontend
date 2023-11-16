a1ce59e0378079930c9caab9d9d37f87815d7c6f
Paniagua <114714690+luizzzito@users.noreply.github.com>
2023-11-16 15:52:34 -0400

    actualizacion de dependencias

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "app/store";
