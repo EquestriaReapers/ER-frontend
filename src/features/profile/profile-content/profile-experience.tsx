f4d3617f7a6b5d69df76c1c6fc4f0ed8ab973dbf
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2023-12-04 22:11:19 -0400

    Cambio middleware

--- src/features/profile/profile-content/profile-experience.tsx
+++ src/features/profile/profile-content/profile-experience.tsx
@@ -1,43 +1,44 @@
-import ShowExperiences from '../experiences/show-experiences/show-experiences'
-import Typography from '@mui/material/Typography'
-import EditExperienceModal from '../experiences/edit-experiences/modal'
-import { Experience } from 'core/profiles/types'
-import { useEffect, useState } from 'react'
-import { getAllExperiences } from '../services/experience.service'
-import { useAuthState } from 'hooks/use-auth-state'
+import ShowExperiences from "../experiences/show-experiences/show-experiences";
+import Typography from "@mui/material/Typography";
+import EditExperienceModal from "../experiences/edit-experiences/modal";
+import { Experience } from "core/profiles/types";
+import { useEffect, useState } from "react";
+import { getAllExperiences } from "../services/experience.service";
+import { useAuthState } from "hooks/use-auth-state";
 
 const ProfileExperience = ({
   isEditable,
-  currentProfileExperience
+  currentProfileExperience,
 }: ProfileExperienceProps) => {
-  const [experiences, setExperiences] = useState<Experience[]>([])
-  const { token } = useAuthState()
+  const [experiences, setExperiences] = useState<Experience[]>([]);
+  const { token } = useAuthState();
   useEffect(() => {
     const fetchExperiences = async () => {
       try {
-        if (!token) return
-        const experiencesData = await getAllExperiences(token)
-        setExperiences(experiencesData)
+        if (!token) return;
+        const experiencesData = await getAllExperiences(token);
+        setExperiences(experiencesData.experience);
+        console.error(experiencesData.experience);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
+    };
 
-    fetchExperiences()
-  }, [])
+    fetchExperiences();
+  }, []);
 
   return (
     <div>
-      <Typography variant='h4'>Experiencia:</Typography>
+      <Typography variant="h4">Experiencia:</Typography>
       {isEditable && <EditExperienceModal />}
       <ShowExperiences experience={currentProfileExperience} />
     </div>
-  )
-}
+  );
+};
 
 export interface ProfileExperienceProps {
-  isEditable: boolean
-  currentProfileExperience: Experience[]
+  isEditable: boolean;
+  currentProfileExperience: Experience[];
 }
 
-export default ProfileExperience
+export default ProfileExperience;
--- src/features/profile/services/experience.service.ts
+++ src/features/profile/services/experience.service.ts
@@ -1,18 +1,21 @@
 import { BACKEND_V1_URL } from "app/config";
 import { BackendError } from "app/exceptions";
 import axios from "axios";
-import { Experience } from "core/profiles/types";  
+import { Experience, Profile } from "core/profiles/types";  
 
-const URL = `${BACKEND_V1_URL}/experience`;  
+const URL = `${BACKEND_V1_URL}/profiles/1`;  
 
-export async function getAllExperiences(token: string): Promise<Experience[]> {
+export async function getAllExperiences(token: string): Promise<Profile> {
   try {
     const response = await axios.get(URL, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
+    console.log(response.data);
+
     return response.data;
+
   } catch (error) {
     throw new BackendError(error);
   }
