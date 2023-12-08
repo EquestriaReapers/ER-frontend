9c8eff5e758d6de87d70b1faddc4a86a34b88bee
pan <114714690+luizzzito@users.noreply.github.com>
2023-12-07 21:26:02 -0400

    minor fixes

--- src/features/profile/experiences/modal/show-experiences-in-modal/modal-content.tsx
+++ src/features/profile/experiences/modal/show-experiences-in-modal/modal-content.tsx
@@ -1,10 +1,20 @@
 import { Box, Typography } from "@mui/material";
 import { modalStyle } from "../styles/styles";
+import ModalShowExperiences from "./show-experiences";
 
 const ShowExperienceModalContent = () => {
   return (
     <Box sx={modalStyle}>
-      <Typography>Mostrar experiencia</Typography>
+      <Box sx={modalStyle}>
+        <Typography>Gestión de experiencias</Typography>
+        <Typography>
+          En esta seccion podras gestionar las experiencias profesionales que
+          has tenido
+        </Typography>
+        <form>
+          <ModalShowExperiences experience={experience} />
+        </form>
+      </Box>
     </Box>
   );
 };
