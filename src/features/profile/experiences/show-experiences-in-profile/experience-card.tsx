4ba199a2bbb429ed958f1d66be72d16efd59358a
Feredev <hector1567xd@gmail.com>
2023-12-09 13:36:42 -0400

    Boyscout rule for ExperienceCard props 🏕

--- src/features/profile/experiences/show-experiences-in-profile/experience-card.tsx
+++ src/features/profile/experiences/show-experiences-in-profile/experience-card.tsx
@@ -1,25 +1,50 @@
 import { Experience } from "core/profiles/types";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
-import { boxStyles, nameStyles, descriptionStyles, roleStyles, inlineStyles, ubicationDateStyles,} from "../styles/styles";
+import {
+  boxStyles,
+  nameStyles,
+  descriptionStyles,
+  roleStyles,
+  inlineStyles,
+  ubicationDateStyles,
+} from "../styles/styles";
 
-const ExperienceCard = ({ item }: { item: Experience }) => {
+const ExperienceCard = ({ item }: Props) => {
   return (
     <div>
-    <Box sx={boxStyles}>
-      <Typography sx={nameStyles} variant="h5">{item.businessName}</Typography>
-      <Box sx={inlineStyles}>
-        <Typography sx={roleStyles} variant="h6">{item.role}</Typography>
-        <Box sx={ubicationDateStyles}>
-          <Typography sx={roleStyles} variant="h6">{item.location}</Typography>
-          <Typography sx={roleStyles} variant="h6">{item.startDate.toLocaleString().slice(0, 10)}</Typography>
-          {item.endDate && (<Typography sx={roleStyles} variant="h6">{item.endDate.toLocaleString().slice(0, 10)}</Typography>)}
+      <Box sx={boxStyles}>
+        <Typography sx={nameStyles} variant="h5">
+          {item.businessName}
+        </Typography>
+        <Box sx={inlineStyles}>
+          <Typography sx={roleStyles} variant="h6">
+            {item.role}
+          </Typography>
+          <Box sx={ubicationDateStyles}>
+            <Typography sx={roleStyles} variant="h6">
+              {item.location}
+            </Typography>
+            <Typography sx={roleStyles} variant="h6">
+              {item.startDate.toLocaleString().slice(0, 10)}
+            </Typography>
+            {item.endDate && (
+              <Typography sx={roleStyles} variant="h6">
+                {item.endDate.toLocaleString().slice(0, 10)}
+              </Typography>
+            )}
+          </Box>
         </Box>
+        <Typography sx={descriptionStyles} variant="h6">
+          {item.description}
+        </Typography>
       </Box>
-      <Typography sx={descriptionStyles} variant="h6">{item.description}</Typography>
-    </Box>
-  </div>
+    </div>
   );
 };
+
+interface Props {
+  item: Experience;
+}
 
 export default ExperienceCard;
