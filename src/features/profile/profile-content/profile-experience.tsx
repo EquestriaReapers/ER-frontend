f5bd9cd477907c047306966c1b2a6a3a4c2da40e
Yisustxz <jesusoortiz2410@gmail.com>
2023-12-09 21:02:33 -0400

    mejore los estilos base que ya estaban hechos de experiencia por anna y agregue el font de Inter

--- src/features/profile/profile-content/profile-experience.tsx
+++ src/features/profile/profile-content/profile-experience.tsx
@@ -1,24 +1,26 @@
-import ShowExperiences from "../experiences/show-experiences/show-experiences";
-import Typography from "@mui/material/Typography";
-import EditProfileModal from "../edit-profile/modal";
-import { Experience } from "core/profiles/types";
-import { boxStyles } from "./styles/styles";
+import ShowExperiences from '../experiences/show-experiences/show-experiences'
+import Typography from '@mui/material/Typography'
+import EditProfileModal from '../edit-profile/modal'
+import { Experience } from 'core/profiles/types'
+import { boxStyles } from './styles/styles'
 
 const ProfileExperience = ({
   isEditable,
-  currentProfileExperience,
+  currentProfileExperience
 }: ProfileExperienceProps) => {
   return (
     <div>
-      <Typography variant="h4" sx={ boxStyles }>Experiencia{isEditable && <EditProfileModal />}</Typography>
+      <Typography variant='h4' sx={boxStyles}>
+        Experiencia{isEditable && <EditProfileModal />}
+      </Typography>
       <ShowExperiences experience={currentProfileExperience} />
     </div>
-  );
-};
-
-export interface ProfileExperienceProps {
-  isEditable: boolean;
-  currentProfileExperience: Experience[];
+  )
 }
 
-export default ProfileExperience;
+export interface ProfileExperienceProps {
+  isEditable: boolean
+  currentProfileExperience: Experience[]
+}
+
+export default ProfileExperience
--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,38 @@
-import ReactDOM from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { store, persistor } from "app/store";
-import Dashboard from "features/dashboard/Dashboard";
-import Profile from "features/profile/Profile";
-import Login from "features/auth/Login";
-import Register from "features/auth/Register";
+import ReactDOM from 'react-dom/client'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import { store, persistor } from 'app/store'
+import Dashboard from 'features/dashboard/Dashboard'
+import Profile from 'features/profile/Profile'
+import Login from 'features/auth/Login'
+import Register from 'features/auth/Register'
+import 'typeface-inter'
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path: "/dashboard",
-    element: <Dashboard />,
+    path: '/dashboard',
+    element: <Dashboard />
   },
   {
-    path: "/profile/:id",
-    element: <Profile />,
+    path: '/profile/:id',
+    element: <Profile />
   },
   {
-    path: "/login",
-    element: <Login />,
+    path: '/login',
+    element: <Login />
   },
   {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+    path: '/register',
+    element: <Register />
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={router} />
     </PersistGate>
   </Provider>
-);
+)
