556fc5eb7588ed835d13e1dc420bd7252a90658e
pan <114714690+luizzzito@users.noreply.github.com>
2023-12-12 11:38:39 -0400

    merge de vainas

--- src/features/profile/edit-profile/use-get-profile-info.tsx
+++ src/features/profile/edit-profile/use-get-profile-info.tsx
@@ -1,10 +1,11 @@
 import { useAuthState } from "hooks/use-auth-state";
 import { useCallback } from "react";
 import { fetchOneProfile } from "../services/profile/fetch-one-profile.service";
-
+import { useErrorToast } from "hooks/use-error-toast";
 
 const useGetProfileInfo = ({ setName, setDescription }: Props) => {
   const { token, user } = useAuthState();
+  const { showErrorToast } = useErrorToast();
   const getUserInfo = useCallback(async () => {
     try {
       if (!token || !user) return;
@@ -12,9 +13,9 @@ const useGetProfileInfo = ({ setName, setDescription }: Props) => {
       setName(data.user.name);
       setDescription(data.description);
     } catch (error) {
-      console.log(error);
+      showErrorToast(error);
     }
-  }, [setDescription, setName, token, user]);
+  }, [setDescription, setName, token, user, showErrorToast]);
 
   return { getUserInfo };
 };
