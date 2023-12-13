e75c606433da874e6fe43a59ff237478d488611d
GreenieWarren <87400557+GreenieWarren@users.noreply.github.com>
2023-12-13 00:52:47 -0400

    Adios ProfileExperience
    Había un profileExperience que no se usaba en profilecontent. Lo comenté por si a las moscas

--- src/features/profile/profile-content/profile-skills.tsx
+++ src/features/profile/profile-content/profile-skills.tsx
@@ -1,27 +1,24 @@
-import Typography from '@mui/material/Typography'
-import ShowSkills from '../skills/show-skills/show-skills'
-import AddSkillsModal from '../skills/add-skills/modal'
-import { Skill } from 'core/profiles/types'
-import { boxStyles } from './styles/styles'
+import Typography from "@mui/material/Typography";
+import ShowSkills from "../skills/show-skills/show-skills";
+import AddSkillsModal from "../skills/add-skills/modal";
+import { Skill } from "core/profiles/types";
+import { boxStyles } from "./styles/styles";
 
-const ProfileSkills = ({
-  isEditable,
-  currentProfileSkills
-}: Props) => {
+const ProfileSkills = ({ isEditable, currentProfileSkills }: Props) => {
   return (
     <div>
-      <Typography sx={boxStyles} variant='h4'>
+      <Typography sx={boxStyles} variant="h4">
         Habilidades
         {isEditable && <AddSkillsModal />}
       </Typography>
-      <ShowSkills skills={currentProfileSkills} isEditable={isEditable} />
+      <ShowSkills skills={currentProfileSkills} />
     </div>
-  )
-}
+  );
+};
 
 export interface Props {
-  isEditable: boolean
-  currentProfileSkills: Skill[]
+  isEditable: boolean;
+  currentProfileSkills: Skill[];
 }
 
-export default ProfileSkills
+export default ProfileSkills;
