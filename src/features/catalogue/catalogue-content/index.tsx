5c57a58a6aecc462c6c248c4d19af004a0f1787a
Feredev <jesusoortiz2410@gmail.com>
2023-12-21 08:31:37 -0400

    avances con la paginacion

--- src/features/catalogue/catalogue-content/index.tsx
+++ src/features/catalogue/catalogue-content/index.tsx
@@ -1,18 +1,46 @@
-import { Profile } from "core/profiles/types";
-import { Box, Pagination, Stack } from "@mui/material";
-import ShowProfiles from "./profiles/show-profiles";
+import { Profile } from 'core/profiles/types'
+import { Box, Pagination, Stack } from '@mui/material'
+import ShowProfiles from './profiles/show-profiles'
 import {
   filtersContainer,
   filtersContainerBackground,
   navBarStyles,
   pageContainer,
   catalogueContainer,
-  searchBarContainer,
-} from "./styles/styles";
-import Filters from "./filters";
-import CatalogueSearchBar from "./components/SearchBar";
+  searchBarContainer
+} from './styles/styles'
+import Filters from './filters'
+import CatalogueSearchBar from './components/SearchBar'
+import { useEffect, useState } from 'react'
+import useGetPaginatedProfiles from './profiles/use-get-paginated-profiles'
 
 const CatalogueContent = ({ profileList }: Props) => {
+  const [profileLists, setProfileLists] = useState<Profile[]>([])
+  const [pagination, setPagination] = useState<Pagination>({
+    itemCount: 0,
+    totalItems: 0,
+    itemsPerPage: 0,
+    totalPages: 0,
+    currentPage: 0,
+    randomSeed: 0
+  })
+
+  const { getProfileList } = useGetPaginatedProfiles({
+    setProfileList: setProfileLists,
+    setPagination
+  })
+
+  const handlePageChange = async () => {
+    try {
+      await getProfileList()
+    } catch (error) {
+      console.error('Error fetching paginated profiles:', error)
+    }
+  }
+
+  useEffect(() => {
+    getProfileList()
+  }, [getProfileList])
   return (
     <>
       <Box sx={navBarStyles}>fakenavbar por ahora</Box>
@@ -25,24 +53,26 @@ const CatalogueContent = ({ profileList }: Props) => {
             </Box>
           </Box>
 
-          <Box sx={{ width: "100%", height: "90%" }}>
+          <Box sx={{ width: '100%', height: '90%' }}>
             <Box sx={searchBarContainer}>
               <CatalogueSearchBar />
             </Box>
             <ShowProfiles profileList={profileList} />
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                my:"46px"
+                display: 'flex',
+                justifyContent: 'center',
+                my: '46px'
               }}
             >
               <Stack spacing={2}>
                 <Pagination
-                  count={100}
-                  shape="rounded"
-                  color="primary"
-                  size="large"
+                  count={pagination.totalPages}
+                  page={pagination.currentPage}
+                  shape='rounded'
+                  color='primary'
+                  size='large'
+                  onChange={handlePageChange}
                 />
               </Stack>
             </Box>
@@ -50,10 +80,10 @@ const CatalogueContent = ({ profileList }: Props) => {
         </Box>
       </Box>
     </>
-  );
-};
+  )
+}
 
 interface Props {
-  profileList: Profile[];
+  profileList: Profile[]
 }
-export default CatalogueContent;
+export default CatalogueContent
