b2591b3d3a863090eb51bda377fd50068ce0ad33
Feredev <jesusoortiz2410@gmail.com>
2023-12-22 20:43:09 -0400

    cambios a detalles del responsive y otras cosas

--- src/features/profile/profile-content/index.tsx
+++ src/features/profile/profile-content/index.tsx
@@ -1,14 +1,14 @@
-import Typography from "@mui/material/Typography";
-import { Box, Button } from "@mui/material";
-import { useAuthState } from "hooks/use-auth-state";
-import EditProfileModal from "../edit-profile/modal";
-import { Profile } from "core/profiles/types";
-import ProfileSkills from "./profile-skills/profile-skills";
-import ProfileExperiences from "./profile-experience/profile-experience";
+import Typography from '@mui/material/Typography'
+import { Box, Button } from '@mui/material'
+import { useAuthState } from 'hooks/use-auth-state'
+import EditProfileModal from '../edit-profile/modal'
+import { Profile } from 'core/profiles/types'
+import ProfileSkills from './profile-skills/profile-skills'
+import ProfileExperiences from './profile-experience/profile-experience'
 
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import EditIcon from "@mui/icons-material/Edit";
-import LanguageIcon from "@mui/icons-material/Language";
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import EditIcon from '@mui/icons-material/Edit'
+import LanguageIcon from '@mui/icons-material/Language'
 import {
   aboutMeSection,
   bannerStyles,
@@ -22,48 +22,48 @@ import {
   nameSectionStyles,
   nameStyles,
   websiteBoxStyles,
-  websiteTitleContainerStyles,
-} from "./styles/styles";
-import useTransformCareerEnum from "hooks/use-transform-career-enum";
+  websiteTitleContainerStyles
+} from './styles/styles'
+import useTransformCareerEnum from 'hooks/use-transform-career-enum'
 const ProfileContent = ({ profile }: ProfileContentProps) => {
-  const loggedUser = useAuthState().user;
-  const isEditable = !!(loggedUser && loggedUser.id == profile.userId);
-  const transformedCareerName = useTransformCareerEnum(profile.mainTitle);
+  const loggedUser = useAuthState().user
+  const isEditable = !!(loggedUser && loggedUser.id == profile.userId)
+  const transformedCareerName = useTransformCareerEnum(profile.mainTitle)
   return (
     <>
       <Box>
         <Box sx={bannerStyles}></Box>
         <Box
           sx={{
-            mx: { xs: "24px", md: "120px" },
-            display: "flex",
-            flexDirection: "column",
-            gap: { xs: "16px", sm: "0px" },
+            mx: { xs: '24px', md: '120px' },
+            display: 'flex',
+            flexDirection: 'column',
+            gap: { xs: '16px', sm: '0px' }
           }}
         >
           <Box
             sx={{
-              width: "100%",
-              mt: "30px",
+              width: '100%',
+              mt: '30px'
             }}
           >
             <Box sx={nameSectionStyles}>
               <Box>
-                <Typography variant="h4" sx={nameStyles}>
-                  {profile.user.name} {profile.user.lastname}{" "}
+                <Typography variant='h4' sx={nameStyles}>
+                  {profile.user.name} {profile.user.lastname}{' '}
                   <Box sx={editIconStyles}>
                     {isEditable && <EditProfileModal />}
                   </Box>
                 </Typography>
-                <Typography variant="h4" sx={mainTitleStyles}>
+                <Typography variant='h4' sx={mainTitleStyles}>
                   {transformedCareerName}
                 </Typography>
               </Box>
-              <Box sx={{ width: { xs: "100%", sm: "30%", md: "15%" } }}>
+              <Box sx={{ width: { xs: '100%', sm: '30%', md: '22%' } }}>
                 <Button
-                  variant="contained"
-                  color="primary"
-                  type="submit"
+                  variant='contained'
+                  color='primary'
+                  type='submit'
                   sx={buttonStyles}
                 >
                   Descargar CV
@@ -73,19 +73,19 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
             <Box sx={aboutMeSection}>
               <Box
                 sx={{
-                  paddingTop: "14px",
-                  paddingBottom: "24px",
-                  gap: "17px",
-                  display: "flex",
-                  flexDirection: "column",
+                  paddingTop: '14px',
+                  paddingBottom: '24px',
+                  gap: '17px',
+                  display: 'flex',
+                  flexDirection: 'column'
                 }}
               >
                 <Typography
-                  variant="h5"
+                  variant='h5'
                   sx={{
-                    fontWeight: "700",
-                    fontFamily: "inter",
-                    fontSize: { xs: "20px", sm: "35px" },
+                    fontWeight: '700',
+                    fontFamily: 'inter',
+                    fontSize: { xs: '20px', sm: '35px' }
                   }}
                 >
                   Sobre Mí
@@ -101,26 +101,26 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
               <Box sx={contactSectionStyles}>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
+                    display: 'flex',
+                    flexWrap: 'wrap'
                   }}
                 >
-                  <Box sx={{ display: "flex" }}>
+                  <Box sx={{ display: 'flex' }}>
                     <Box sx={locationBoxStyles}>
                       <LocationOnIcon />
                       <Typography sx={contactTitlesStyles}>
                         Ubicación
                       </Typography>
                     </Box>
-                    <Box sx={{ display: "flex" }}>
+                    <Box sx={{ display: 'flex' }}>
                       <EditIcon />
                     </Box>
                   </Box>
 
                   <Typography
                     sx={{
-                      marginLeft: "10px",
-                      fontFamily: "inter",
+                      marginLeft: '10px',
+                      fontFamily: 'inter'
                     }}
                   >
                     Puerto Ordaz, Venezuela
@@ -128,11 +128,11 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
                 </Box>
                 <Box sx={websiteBoxStyles}>
                   <Box sx={websiteTitleContainerStyles}>
-                    <LanguageIcon />{" "}
+                    <LanguageIcon />{' '}
                     <Typography sx={contactTitlesStyles}>Website</Typography>
                   </Box>
 
-                  <Typography sx={{ marginLeft: "10px", fontFamily: "inter" }}>
+                  <Typography sx={{ marginLeft: '10px', fontFamily: 'inter' }}>
                     www.abcdefge.com
                   </Typography>
                 </Box>
@@ -141,9 +141,9 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
           </Box>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: { xs: "29px", sm: "37px" },
+              display: 'flex',
+              flexDirection: 'column',
+              gap: { xs: '29px', sm: '37px' }
             }}
           >
             <ProfileSkills
@@ -156,11 +156,11 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
               currentProfileExperience={profile.experience}
             />
           </Box>
-          <Box sx={{ display: { sm: "none" }, width: { xs: "100%" } }}>
+          <Box sx={{ display: { sm: 'none' }, width: { xs: '100%' } }}>
             <Button
-              variant="contained"
-              color="primary"
-              type="submit"
+              variant='contained'
+              color='primary'
+              type='submit'
               sx={buttonStyles}
             >
               Descargar CV
@@ -169,10 +169,10 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
         </Box>
       </Box>
     </>
-  );
-};
+  )
+}
 
 export interface ProfileContentProps {
-  profile: Profile;
+  profile: Profile
 }
-export default ProfileContent;
+export default ProfileContent
