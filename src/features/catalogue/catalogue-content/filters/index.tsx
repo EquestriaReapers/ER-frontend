15c2f5c25af0715693bad6315e2615d11ff8956c
jesus <jdrch62@gmail.com>
2023-12-23 15:59:25 -0400

    Tarea de estilado de filtros liberada te digo bai bai chao sayonara

--- src/features/catalogue/catalogue-content/filters/index.tsx
+++ src/features/catalogue/catalogue-content/filters/index.tsx
@@ -2,13 +2,47 @@ import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
 import Typography from "@mui/material/Typography";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 import { typographyStyles } from "./styles/styles";
+import { useState } from "react";
+import Chip from "@mui/material/Chip";
 
 const Filters = () => {
+
+  const [selectedCarreras, setSelectedCarreras] = useState([]);
+
+
+  const handleCarreraChange = (event) => {
+    const value = event.target.value;
+    setSelectedCarreras((prevSelected) => {
+      if (prevSelected.includes(value)) {
+        return prevSelected.filter((carrera) => carrera !== value);
+      } else {
+        return [...prevSelected, value];
+      }
+    });
+    console.log(value);
+  };
+
+  const handleCheckboxClick = (value) => {
+    setSelectedCarreras((prevSelected) => {
+      if (prevSelected.includes(value)) {
+        return prevSelected.filter((carrera) => carrera !== value);
+      } else {
+        return [...prevSelected, value];
+      }
+    });
+    console.log(value);
+  };
+
+
+
   return (
     <>
       <Typography sx={{ fontWeight: "700", fontSize: "1.25rem" }}>
-        Filtros
+        Filtrar resultados
       </Typography>
 
       <Typography
@@ -19,7 +53,7 @@ const Filters = () => {
       >
         Carrera
       </Typography>
-      <FormGroup>
+    { /* <FormGroup>
         <FormControlLabel
           control={<Checkbox defaultChecked />}
           label="Carrera 1"
@@ -27,9 +61,49 @@ const Filters = () => {
         <FormControlLabel control={<Checkbox />} label="Carrera 2" />
         <FormControlLabel control={<Checkbox />} label="Carrera 3" />
         <FormControlLabel control={<Checkbox />} label="Carrera 4" />
-      </FormGroup>
+      </FormGroup>*/}
 
-      <Typography sx={typographyStyles}>select de carrera</Typography>
+      <Select 
+        label="Carrera"
+        multiple
+        displayEmpty
+        value={selectedCarreras}
+        //onChange={handleCarreraChange}
+        renderValue={(selected) => "Todas las carreras"}
+        sx={{ minWidth: 120, marginBottom: 2 }}
+      >
+        <MenuItem value="" disabled>
+          Todas las carreras
+        </MenuItem>
+        <MenuItem value="Carrera 1" onClick={() => handleCheckboxClick("Carrera 1")}>
+          <Checkbox checked={selectedCarreras.includes("Carrera 1")} />
+          Carrera 1
+        </MenuItem>
+        <MenuItem value="Carrera 2" onClick={() => handleCheckboxClick("Carrera 2")}>
+          <Checkbox checked={selectedCarreras.includes("Carrera 2")} />
+          Carrera 2
+        </MenuItem>
+        <MenuItem value="Carrera 3" onClick={() => handleCheckboxClick("Carrera 3")}>
+          <Checkbox checked={selectedCarreras.includes("Carrera 3")} />
+          Carrera 3
+        </MenuItem>
+        <MenuItem value="Carrera 4" onClick={() => handleCheckboxClick("Carrera 4")}>
+          <Checkbox checked={selectedCarreras.includes("Carrera 4")} />
+          Carrera 4
+        </MenuItem>
+      </Select>
+      <Box>
+        {selectedCarreras.map((carrera) => (
+          <Chip
+            key={carrera}
+            label={carrera}
+            onDelete={() => handleCheckboxClick(carrera)}
+            color="primary"
+            sx={{ marginRight: 1, marginBottom: 1 }}
+
+          />
+        ))}
+      </Box>
       <Typography sx={typographyStyles}>Habilidades</Typography>
     </>
   );
