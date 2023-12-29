d9a0502db0381a58b3a793c385f2ffd7d74a2d4c
Feredev <hector1567xd@gmail.com>
2023-12-28 21:59:59 -0400

    No se que es esta basura pero se sube

--- src/features/profile/edit-profile/use-get-profile-info.tsx
+++ src/features/profile/edit-profile/use-get-profile-info.tsx
@@ -2,19 +2,23 @@ import { useAuthState } from "hooks/use-auth-state";
 import { useCallback } from "react";
 import { fetchOneProfile } from "../services/profile/fetch-one-profile.service";
 
-
-const useGetProfileInfo = ({ setName, setDescription }: Props) => {
-  const { token, user } = useAuthState();
+const useGetProfileInfo = ({
+  setName,
+  setDescription,
+  setMainTitle,
+}: Props) => {
+  const { user } = useAuthState();
   const getUserInfo = useCallback(async () => {
     try {
-      if (!token || !user) return;
-      const data = await fetchOneProfile(token, user.id);
+      if (!user) return;
+      const data = await fetchOneProfile(user.id);
       setName(data.user.name);
       setDescription(data.description);
+      setMainTitle(data.mainTitle);
     } catch (error) {
       console.log(error);
     }
-  }, [setDescription, setName, token, user]);
+  }, [setDescription, setMainTitle, setName, user]);
 
   return { getUserInfo };
 };
@@ -24,4 +28,5 @@ export default useGetProfileInfo;
 interface Props {
   setName: (name: string) => void;
   setDescription: (description: string) => void;
+  setMainTitle: (mainTitle: string) => void;
 }
