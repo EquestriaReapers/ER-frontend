033d6c0d3aa426b571c8379fc67094976c610fa5
pan <114714690+luizzzito@users.noreply.github.com>
2023-12-29 18:19:11 -0400

    correcciones

--- src/features/profile/services/profile/export-curriculum-pdf.service.ts
+++ src/features/profile/services/profile/export-curriculum-pdf.service.ts
@@ -2,14 +2,11 @@ import axios from "axios";
 import { BackendError } from "app/exceptions";
 import { PROFILES_URL } from "./config/config";
 
-export async function exportCurriculumPDF(token: string, profileId: number) {
+export async function exportCurriculumPDF(profileId: number) {
   try {
     const response = await axios.get(
       `${PROFILES_URL}/export-pdf/${profileId}`,
       {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
         responseType: "blob",
       }
     );
