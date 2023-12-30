c47be2ad995ac59f6789d613ae22ef9b8bd7d5e9
jesus <jdrch62@gmail.com>
2023-12-30 11:21:27 -0400

    Mas avances en refactorizacion

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import curriculum from "./images/curriculum.jpeg";
-import background from "./images/background.jpeg";
 import SearchIcon from '@mui/icons-material/Search'; 
 import InputAdornment from '@mui/material/InputAdornment'; 
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
@@ -22,14 +21,27 @@ import {
     useSearchBoxStyles,
     SearchButtomStyles,
     InsideSearchBarTextSectionStyles,
+    InsideSearchBarSectionTypographyStyles,
+    useGetCVSectionStyles,
+    useInsideGetCVSectionStyles,
+    useInsideGetCVTypographytyles,
+    useInsideGetCVSectionTextStyles,
+    useGetCVBoxtStyles,
+    useGetCVImagetStyles,
+    SecondSectionStyles,
+    useInsideSecondSectionStyles,
+    SecondSectionFigureBoxStyles,
+    useSecondSectionFigureStyles,
+    SecondSectionFigureBackgroundStyles,
+    useSecondSectionDownloadCVSectionStyles,
 } from "./HomeStyles";
 
 const Home = () => {
 
     const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
     const isMediumScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
-    const isExtraSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down(550));
     const isExtraExtraSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down(400));
+    const isMinimunWidthScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down(300));
 
     const HomePageStyles = useHomePageContainerStyles();
     const MainContainerStyles = useMainContainerStyles();
@@ -38,6 +50,15 @@ const Home = () => {
     const SearchEgresatesSectionStyles = useSearchEgresatesSectionStyles();
     const SearchEgresatesTypographyStyles = useSearchEgresatesTypographyStyles();
     const SearchBoxStyles = useSearchBoxStyles();
+    const GetCVSectionStyles = useGetCVSectionStyles();
+    const InsideGetCVSectionStyles = useInsideGetCVSectionStyles();
+    const InsideGetCVTypographyStyles = useInsideGetCVTypographytyles();
+    const InsideGetCVSectionTextStyles = useInsideGetCVSectionTextStyles();
+    const GetCVBoxStyles = useGetCVBoxtStyles();
+    const GetCVImageStyles = useGetCVImagetStyles();
+    const InsideSecondSectionStyles = useInsideSecondSectionStyles();
+    const SecondSectionFigureStyles = useSecondSectionFigureStyles();
+    const SecondSectionDownloadCVSectionStyles = useSecondSectionDownloadCVSectionStyles();
 
     return (
         <Box sx={HomePageStyles}>
@@ -88,7 +109,7 @@ const Home = () => {
                                     </Button>
                                 </Box>
                                 <Box sx={InsideSearchBarTextSectionStyles}>
-                                    <Typography sx={{fontWeight:'500', fontSize:'1rem', textAlign:'center'}}>
+                                    <Typography sx={InsideSearchBarSectionTypographyStyles}>
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                                         Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur
                                     </Typography>
@@ -96,16 +117,16 @@ const Home = () => {
                             </Box>
 
                         </Box>
-                        <Box sx={{width:'100%', height:'50%', display: isMediumScreen ? 'grid' : 'flex', alignItems:'center'}}>
-                            <Box sx={{width: isMediumScreen ? '100%' : '45%', height:'50%', margin: isMediumScreen ? 'auto' : 0}}>
+                        <Box sx={GetCVSectionStyles}>
+                            <Box sx={InsideGetCVSectionStyles}>
                                 <Box>
                                     <Box>
-                                        <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom:'40px', textAlign: isMediumScreen ? 'center' : 'left'}}>
+                                        <Typography sx={InsideGetCVTypographyStyles}>
                                             Obtén tu CV
                                         </Typography>
                                     </Box>
                                     <Box>
-                                        <Typography sx={{fontWeight:'500', fontSize:'0.9rem', textAlign: isMediumScreen ? 'center' : 'left'}}>
+                                        <Typography sx={InsideGetCVSectionTextStyles}>
                                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, 
                                             molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum 
                                             numquam 
@@ -114,8 +135,8 @@ const Home = () => {
                                 </Box>
 
                             </Box>
-                            <Box sx={{width:'55%', height:'50%',  transform: 'rotate(3deg)', display:'flex', justifyContent:'center', margin: isMediumScreen ? 'auto' : 0}}>
-                                <img src={curriculum} alt="" style={ {minHeight: isExtraSmallScreen ? '0px' : (isSmallScreen ? '480px': (isMediumScreen ? '600px' : '450px')), height: isExtraExtraSmallScreen ? '60vh': (isMediumScreen ? '80vh': '50vh'), maxHeight: isExtraExtraSmallScreen ? '400px' : isExtraSmallScreen ? '480px' : (isSmallScreen ? '520px' : (isMediumScreen ? '650px' : '600px')), width: isExtraExtraSmallScreen ? '85vw': 'auto', border:'1px solid black'}} />
+                            <Box sx={GetCVBoxStyles}>
+                                <img src={curriculum} alt="" style={GetCVImageStyles} />
                             </Box>
 
                         </Box>
@@ -123,34 +144,16 @@ const Home = () => {
                     
                 </Box>
 
-                <Box sx={{
-                    width: '100%', 
-                    height:'22%', 
-                    position: 'relative',
-                    '&::before': {
-                    content: '""',
-                    position:'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    backgroundImage: `url(${background})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    filter: 'blur(3.3px)',
-                    zIndex: -1,
-                    display: 'flex'
-                    },
-                }}>
-                    <Box sx={{width: isMediumScreen ? '90%' : '80%', height: '90%', display: isSmallScreen ? 'grid' : 'flex', margin:'auto', position:'relative', zIndex:1, maxWidth: '2000px'}}>
-                        <Box sx={{width:'50%', height:'100%', display:'flex', alignItems:'center', margin: 'auto'}}>
-                            <Box sx={{height:'300px', width:'250px', backgroundColor:'#72C4E8',  transform: 'rotate(8deg)', display:'flex', alignItems:'center', margin: isSmallScreen ? 'auto auto 0 auto':'auto'}}>
-                                <Box sx={{height:'110%', width:'98%', backgroundColor:'white', transform: 'rotate(-9deg)', marginBottom:'40px'}}>
+                <Box sx={SecondSectionStyles}>
+                    <Box sx={InsideSecondSectionStyles}>
+                        <Box sx={SecondSectionFigureBoxStyles}>
+                            <Box sx={SecondSectionFigureStyles}>
+                                <Box sx={SecondSectionFigureBackgroundStyles}>
 
                                 </Box>
                             </Box>
                         </Box>
-                        <Box sx={{width: isSmallScreen ? '100%' :'50%', height:'40%', margin: isSmallScreen ? 'auto' : 'auto 0 60px 0'}}>
+                        <Box sx={SecondSectionDownloadCVSectionStyles}>
                             <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom:'40px', color:'#FFFFFF', textAlign: isSmallScreen ? 'center' :'left'}}>Lorem</Typography>
                             <Typography sx={{marginBottom:'40px', fontWeight:'500', fontSize:'0.9rem', color:'#FFFFFF', textAlign: isSmallScreen ? 'center' : 'left'}}>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, 
@@ -185,13 +188,13 @@ const Home = () => {
                             </Box>
                         </Box>
                         <Box sx={{height:'70%', width: '100%', display: isMediumScreen ? 'grid' : 'flex'}}>
-                            <Box sx={{width: '50%', height:'100%', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', margin: isMediumScreen ? 'auto' : 0}}>
+                            <Box sx={{width: '50%', height:'100%', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', margin: isMediumScreen ? 'auto' : 0, marginTop: isSmallScreen ? '-10px': 'auto'}}>
                                 <Box sx={{width: isExtraExtraSmallScreen ? '200px' : '250px', height: isExtraExtraSmallScreen ? '200px':  '250px', backgroundColor: '#FFE298', transform: 'skew(-2.8deg) rotate(90.5deg)', display: 'flex', justifyContent:'center', alignItems: 'center', marginBottom: '40px'  }}>
                                     <Box sx={{backgroundColor:'white', height:'96%', width:'96%', transform: 'rotate(-2.5deg)' }}>
 
                                     </Box>
                                 </Box>
-                                <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom:'40px'}}>
+                                <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom: isMinimunWidthScreen ? '20px':'40px'}}>
                                     Lorem
                                 </Typography>
                                 <Typography sx={{fontWeight:'500', fontSize:'0.9rem', textAlign: 'center'}}>
@@ -200,14 +203,14 @@ const Home = () => {
                                     molestiae quas vel sint commodi 
                                 </Typography>
                             </Box>
-                            <Box sx={{width: '50%', height:'100%', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', margin: isMediumScreen ? '40px auto auto auto' : 0}}>
-                                <Box sx={{width: isExtraExtraSmallScreen ? '210px' : '300px', height: isExtraExtraSmallScreen ? '200px' : '250px', backgroundColor: '#A6D6A8', transform: 'skew(-4deg)', display: 'flex', justifyContent:'center', alignItems: 'center', marginBottom: '25px'  }}>
+                            <Box sx={{width: '50%', height:'100%', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', margin: isSmallScreen ? '10px auto auto auto' : (isMediumScreen ? '40px auto auto auto' : 0)}}>
+                                <Box sx={{width: isExtraExtraSmallScreen ? '210px' : '300px', height: isExtraExtraSmallScreen ? '200px' : '250px', backgroundColor: '#A6D6A8', transform: 'skew(-4deg)', display: 'flex', justifyContent:'center', alignItems: 'center', marginBottom: '40px'  }}>
                                     <Box sx={{backgroundColor:'white', height:'94%', width:'94%', transform: 'skew(5deg)' }}>
 
                                     </Box>
                                 </Box>
 
-                                <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom:'40px'}}>
+                                <Typography sx={{fontWeight:'600', fontSize:'2rem', marginBottom: isMinimunWidthScreen ? '20px' : '40px'}}>
                                     Lorem
                                 </Typography>
                                 <Typography sx={{fontWeight:'500', fontSize:'0.9rem', textAlign: 'center'}}>
