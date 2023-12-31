e6b277dd1be1f8060a9af2c013c9fe1e6e883c91
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2023-12-30 20:47:39 -0400

    actualizando rutas del navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,28 +1,51 @@
 import { Button, Typography, Toolbar, Box, AppBar } from "@mui/material/";
 import { navbarStyles } from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
+import { AuthState } from "core/auth/store/types";
+import { User } from "core/users/types";
+import useRedirectWhenUnlogged from "hooks/use-redirect-when-unlogged";
+import { logout } from "core/auth/store/auth-slice";
 
 const Navbar = () => {
+  const user = useCurrentUser();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useRedirectWhenUnlogged();
+
+  if (!user) return null;
+
+  const onLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar sx={navbarStyles}>
-          <Link to="/dashboard">
+          <Link to="/">
             <Typography>Home</Typography>
           </Link>
-          <Link to="/perfil">
+          <Link to={`/profile/${user.id}`}>
             <Typography>Perfil</Typography>
           </Link>
-          <Link to="/catalogo">
+          <Link to={`/catalogue`}>
             <Typography>Catálogo</Typography>
           </Link>
-          <Link to="/logout">
+          <Button onClick={onLogout}>
             <Typography>Logout</Typography>
-          </Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
   );
 };
+
+function useCurrentUser(): User | null {
+  return useSelector<{ auth: AuthState }>((state) => state.auth.user) as User;
+}
 
 export default Navbar;
