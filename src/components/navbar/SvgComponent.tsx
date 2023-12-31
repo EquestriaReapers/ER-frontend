8320dc0ec6dbfcf6df9121046af8ccb67382ac6c
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2023-12-30 22:41:52 -0400

    icono

--- src/components/navbar/SvgComponent.tsx
+++ src/components/navbar/SvgComponent.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from "react"
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="65px"
+    width="70px"
     height="65px"
     viewBox="0 0 1920 1394"
     {...props}
