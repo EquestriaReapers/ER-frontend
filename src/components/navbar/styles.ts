aa4709ba6178b08b52001b4dd8bd47c6cf8debf0
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2023-12-30 22:43:22 -0400

    sombras

--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -2,6 +2,7 @@ export const navbarStyles = {
     background: 'white',
     display: 'flex',
     justifyContent: 'space-between',
+    boxShadow: { xs: "none", sm: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" },
 };
 
 export const titleStyles = {
