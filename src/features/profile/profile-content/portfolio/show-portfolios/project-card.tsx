2a992eb02b8b26592eee3e25309e748eca83fa6d
pan <114714690+luizzzito@users.noreply.github.com>
2024-01-03 13:07:25 -0400

    Update project-card.tsx

--- src/features/profile/profile-content/portfolio/show-portfolios/project-card.tsx
+++ src/features/profile/profile-content/portfolio/show-portfolios/project-card.tsx
@@ -4,83 +4,85 @@ import {
   CardContent,
   Typography,
   Box,
-  Modal
-} from '@mui/material'
-import { Portfolio } from 'core/profiles/types'
-import { useState } from 'react'
-import ProjectInfoModal from './project-info-modal'
+  Modal,
+} from "@mui/material";
+import { Portfolio } from "core/profiles/types";
+import { useState } from "react";
+import ProjectInfoModal from "./project-info-modal";
 
 function ProjectCard({ project }: Props) {
-  const [hover, setHover] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
-  const showPortfolioText = () => setHover(true)
-  const hidePortfolioText = () => setHover(false)
-  const limitWords = (text: string, limit: number) => {
-    const words = text.split(' ')
-    return words.length > limit ? `${words.slice(0, limit).join(' ')}...` : text
-  }
+  const [hover, setHover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const showPortfolioText = () => setHover(true);
+  const hidePortfolioText = () => setHover(false);
 
-  const openModal = () => setIsOpen(true)
-  const closeModal = () => setIsOpen(false)
+  const limitWords = (text: string, limit: number) => {
+    const words = text.split(" ");
+    return words.length > limit
+      ? `${words.slice(0, limit).join(" ")}...`
+      : text;
+  };
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
   return (
     <Card
       sx={{
-        maxWidth: '300px',
-        width: '300px',
-        maxHeight: '1000',
-        height: '300px',
-        textDecoration: 'none',
-        cursor: 'pointer'
+        maxWidth: "300px",
+        width: "300px",
+        maxHeight: "1000",
+        height: "300px",
+        textDecoration: "none",
+        cursor: "pointer",
       }}
       onMouseEnter={showPortfolioText}
       onMouseLeave={hidePortfolioText}
       onClick={openModal}
     >
       <CardMedia
-        sx={{ height: '300px', position: 'relative' }}
+        sx={{ height: "300px", position: "relative" }}
         image={project.imagePrincipal}
         title={project.title}
       >
-        <Box sx={{ width: '300px' }}></Box>
+        <Box sx={{ width: "300px" }}></Box>
         {hover && (
           <CardContent
             sx={{
-              backgroundColor: 'rgba(0, 0, 0, 0.58)  ',
-              py: '32px',
-              px: '32px',
-              position: 'absolute',
+              backgroundColor: "rgba(0, 0, 0, 0.58)  ",
+              py: "32px",
+              px: "32px",
+              position: "absolute",
               top: 0,
               bottom: 0,
               left: 0,
               right: 0,
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '22px'
+              display: "flex",
+              flexDirection: "column",
+              gap: "22px",
             }}
           >
             <Typography
               sx={{
-                color: '#FFF',
-                fontFamily: 'inter',
-                fontSize: '24px',
-                fontStyle: 'normal',
-                fontWeight: '500',
-                lineHeight: 'normal',
-                textTransform: 'capitalize'
+                color: "#FFF",
+                fontFamily: "inter",
+                fontSize: "24px",
+                fontStyle: "normal",
+                fontWeight: "500",
+                lineHeight: "normal",
+                textTransform: "capitalize",
               }}
             >
               {project.title}
             </Typography>
             <Typography
               sx={{
-                color: '#FFF',
-                fontFamily: 'inter',
-                fontSize: '16px',
-                fontStyle: 'normal',
-                fontWeight: '400',
-                lineHeight: 'normal',
-                textTransform: 'capitalize'
+                color: "#FFF",
+                fontFamily: "inter",
+                fontSize: "16px",
+                fontStyle: "normal",
+                fontWeight: "400",
+                lineHeight: "normal",
+                textTransform: "capitalize",
               }}
             >
               {limitWords(project.description, 40)}
@@ -92,10 +94,10 @@ function ProjectCard({ project }: Props) {
         <ProjectInfoModal />
       </Modal>
     </Card>
-  )
+  );
 }
 
 interface Props {
-  project: Portfolio
+  project: Portfolio;
 }
-export default ProjectCard
+export default ProjectCard;
