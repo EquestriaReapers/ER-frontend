85f4f3026cfa0fc71d7fa89e57a93f3a611fd438
pan <114714690+luizzzito@users.noreply.github.com>
2024-01-03 19:51:02 -0400

    PUSHEA HIJO DE PUTA

--- src/core/portfolio/edit-project.service.ts
+++ src/core/portfolio/edit-project.service.ts
@@ -32,7 +32,6 @@ export async function updateProfileProject(
         },
       }
     );
-    console.log(response.data);
     return response.data;
   } catch (error) {
     throw new BackendError(error);
