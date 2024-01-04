3bc0bd5d8ae4e4f26b90fc1d50d29109a31941fa
pan <114714690+luizzzito@users.noreply.github.com>
2024-01-04 19:10:15 -0400

    correcciones de estilos

--- src/features/auth/NewPassword.tsx
+++ src/features/auth/NewPassword.tsx
@@ -3,61 +3,57 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { FunctionComponent } from "react";
-import { 
-    NewPasswordContainerStyles, 
-    NewPasswordInsideContainerStyles, 
-    useCenterBoxStyles, 
-    InsideCenterBoxStyles, 
-    useRecoverPasswordTypographyStyles,
-    useRecoverPasswordTextStyles,
-    SearchBarBoxStyles,
-    useNewPasswordTypographyStyles,
-    SearchBarTextFieldStyles,
-    useConfirmPasswordStyles,
+import {
+  NewPasswordContainerStyles,
+  NewPasswordInsideContainerStyles,
+  useCenterBoxStyles,
+  InsideCenterBoxStyles,
+  useRecoverPasswordTypographyStyles,
+  useRecoverPasswordTextStyles,
+  SearchBarBoxStyles,
+  useNewPasswordTypographyStyles,
+  SearchBarTextFieldStyles,
+  useConfirmPasswordStyles,
 } from "./styles/NewPasswordStyles";
-import "../../styles/index.css"
+import "../../styles/index.css";
 
 const NewPassword: FunctionComponent = () => {
+  const CenterBoxStyles = useCenterBoxStyles();
+  const RecoverPasswordTypographyStyles = useRecoverPasswordTypographyStyles();
+  const RecoverPasswordTextStyles = useRecoverPasswordTextStyles();
+  const NewPasswordTypographyStyles = useNewPasswordTypographyStyles();
+  const ConfirmPasswordStyles = useConfirmPasswordStyles();
 
-    const CenterBoxStyles = useCenterBoxStyles();
-    const RecoverPasswordTypographyStyles = useRecoverPasswordTypographyStyles();
-    const RecoverPasswordTextStyles = useRecoverPasswordTextStyles();
-    const NewPasswordTypographyStyles = useNewPasswordTypographyStyles();
-    const ConfirmPasswordStyles = useConfirmPasswordStyles();
-
-    return (
-        <Box sx={NewPasswordContainerStyles}>
-            <Box sx= {NewPasswordInsideContainerStyles}>
-                <Box sx={CenterBoxStyles}>
-                    <Box sx={InsideCenterBoxStyles}>
-                        <Typography sx={RecoverPasswordTypographyStyles}>Nueva contraseña</Typography>
-                        <Typography sx={RecoverPasswordTextStyles}>
-                            Ingrese su nueva contraseña
-                        </Typography>
-                        <Box sx={SearchBarBoxStyles}>
-                            <Typography sx={NewPasswordTypographyStyles}>
-                                Contraseña
-                            </Typography>
-                            <TextField 
-                                variant='outlined'
-                                sx={{...SearchBarTextFieldStyles, marginBottom:'30px'}}
-                            />
-                            <Typography sx={NewPasswordTypographyStyles}>
-                                Confirmar contraseña
-                            </Typography>
-                            <TextField 
-                                variant='outlined'
-                                sx={SearchBarTextFieldStyles}
-                            />
-                        </Box>
-                        <Button sx={ConfirmPasswordStyles}>Confirmar</Button>
-                    </Box>
-                </Box>
+  return (
+    <Box sx={NewPasswordContainerStyles}>
+      <Box sx={NewPasswordInsideContainerStyles}>
+        <Box sx={CenterBoxStyles}>
+          <Box sx={InsideCenterBoxStyles}>
+            <Typography sx={RecoverPasswordTypographyStyles}>
+              Nueva contraseña
+            </Typography>
+            <Typography sx={RecoverPasswordTextStyles}>
+              Ingrese su nueva contraseña
+            </Typography>
+            <Box sx={SearchBarBoxStyles}>
+              <Typography sx={NewPasswordTypographyStyles}>
+                Contraseña
+              </Typography>
+              <TextField
+                variant="outlined"
+                sx={{ ...SearchBarTextFieldStyles, marginBottom: "30px" }}
+              />
+              <Typography sx={NewPasswordTypographyStyles}>
+                Confirmar contraseña
+              </Typography>
+              <TextField variant="outlined" sx={SearchBarTextFieldStyles} />
             </Box>
+            <Button sx={ConfirmPasswordStyles}>Confirmar</Button>
+          </Box>
         </Box>
-
-    );
+      </Box>
+    </Box>
+  );
 };
-  
 
 export default NewPassword;
--- src/features/auth/styles/ForgotPaswordStyles.tsx
+++ src/features/auth/styles/ForgotPaswordStyles.tsx
@@ -1,174 +1,170 @@
-import background from "../images/background.jpeg"
+import background from "../images/background.jpeg";
 import { useMediaQuery, useTheme } from "@mui/material";
 
-const XS_SCREEN_LARGE_FONT_SIZE = '1.8rem';
-const XS_SCREEN_MEDIUM_FONT_SIZE = '0.8rem';
-const XS_SCREEN_SMALL_FONT_SIZE = '0.75rem';
+const XS_SCREEN_LARGE_FONT_SIZE = "1.8rem";
+const XS_SCREEN_MEDIUM_FONT_SIZE = "0.8rem";
+const XS_SCREEN_SMALL_FONT_SIZE = "0.75rem";
 
-const LG_SCREEN_LARGE_FONT_SIZE = '2rem';
-const LG_SCREEN_MEDIUM_FONT_SIZE = '1rem';
-const LG_SCREEN_SMALL_FONT_SIZE = '0.95rem';
-
+const LG_SCREEN_LARGE_FONT_SIZE = "2rem";
+const LG_SCREEN_MEDIUM_FONT_SIZE = "1rem";
+const LG_SCREEN_SMALL_FONT_SIZE = "0.95rem";
 
 export const ForgotPasswordContainerStyles = {
-    width: '100%', 
-    height: '100%',  
-    minHeight: '650px', 
-    minWidth:'250px',
-    display:'flex', 
-    alignItems:'center'
-}
+  width: "100%",
+  height: "100%",
+  minHeight: "650px",
+  minWidth: "250px",
+  display: "flex",
+  alignItems: "center",
+};
 
 export const ForgotPasswordInsideContainerStyles = {
-    width: '100%', 
-    height:'100%', 
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    overflow:'hidden',
+  width: "100%",
+  height: "100%",
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  overflow: "hidden",
 
-    '&::before': {
+  "&::before": {
     content: '""',
-    position:'absolute',
-    top: '-3.3px',
-    left: '-3.3px',
-    right: '-3.3px',
-    bottom: '-3.3px',
-    width: '100%',
-    height: '100%',
+    position: "absolute",
+    top: "-3.3px",
+    left: "-3.3px",
+    right: "-3.3px",
+    bottom: "-3.3px",
+    width: "100%",
+    height: "100%",
     backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    filter: 'blur(3.3px)',
-    border : '3.3px solid black',
-    overflow: 'hidden',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    filter: "blur(3.3px)",
+    border: "3.3px solid black",
+    overflow: "hidden",
     zIndex: -1,
-    },
-
-}
-
+  },
+};
 
 export const useCenterBoxStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down(650));
 
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down(650));
+  const CenterBoxStyles = {
+    height: "600px",
+    width: isSmallScreen ? "80%" : "600px",
+    backgroundColor: "white",
+    margin: "auto",
+    display: "flex",
+    alignItems: "center",
+    borderRadius: "6px",
+  };
 
-
-    const CenterBoxStyles = {
-        height: '600px', 
-        width: isSmallScreen ? '80%' :'600px', 
-        backgroundColor:'white', 
-        margin: 'auto', 
-        display:'flex', 
-        alignItems:'center'
-    };
-  
-    return CenterBoxStyles;
+  return CenterBoxStyles;
 };
 
 export const InsideCenterBoxStyles = {
-    width:'80%', 
-    height: '80%', 
-    margin: 'auto', 
-    display: 'flex', 
-    flexDirection:'column'
-}
+  width: "80%",
+  height: "80%",
+  margin: "auto",
+  display: "flex",
+  flexDirection: "column",
+};
 
 export const useRecoverPasswordTypographyStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
 
-    const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
+  const RecoverPasswordTypographyStyles = {
+    fontSize: isExtraSmallScreen
+      ? XS_SCREEN_LARGE_FONT_SIZE
+      : LG_SCREEN_LARGE_FONT_SIZE,
+    fontWeight: "700",
+    marginBottom: "30px",
+    color: "#000000",
+  };
 
-
-    const RecoverPasswordTypographyStyles = {
-        fontSize:  isExtraSmallScreen ? XS_SCREEN_LARGE_FONT_SIZE : LG_SCREEN_LARGE_FONT_SIZE, 
-        fontWeight:'700', marginBottom: '30px',
-        color: '#000000'
-    }
-  
-    return RecoverPasswordTypographyStyles;
+  return RecoverPasswordTypographyStyles;
 };
 
 export const useRecoverPasswordTextStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
 
-    const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
+  const RecoverPasswordTextStyles = {
+    fontSize: isExtraSmallScreen
+      ? XS_SCREEN_MEDIUM_FONT_SIZE
+      : LG_SCREEN_MEDIUM_FONT_SIZE,
+    color: "#575757",
+  };
 
-
-    const RecoverPasswordTextStyles = {
-        fontSize: isExtraSmallScreen ? XS_SCREEN_MEDIUM_FONT_SIZE : LG_SCREEN_MEDIUM_FONT_SIZE,
-        color: '#575757',
-    }
-  
-    return RecoverPasswordTextStyles;
+  return RecoverPasswordTextStyles;
 };
 
-
 export const SearchBarBoxStyles = {
-    margin: 'auto 0 auto 0'
-}
+  margin: "auto 0 auto 0",
+};
 
 export const useEmailTypographyStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
 
-    const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
+  const EmailTypographyStyles = {
+    fontSize: isExtraSmallScreen
+      ? XS_SCREEN_MEDIUM_FONT_SIZE
+      : LG_SCREEN_MEDIUM_FONT_SIZE,
+    marginBottom: "4px",
+    color: "#979899",
+  };
 
-
-    const EmailTypographyStyles = {
-        fontSize: isExtraSmallScreen ? XS_SCREEN_MEDIUM_FONT_SIZE : LG_SCREEN_MEDIUM_FONT_SIZE, 
-        marginBottom: '4px', 
-        color: '#979899' 
-    }
-  
-    return EmailTypographyStyles;
+  return EmailTypographyStyles;
 };
 
 export const SearchBarTextFieldStyles = {
-    width:'100%', 
-    marginBottom:'15px'
-}
-
+  width: "100%",
+  marginBottom: "15px",
+};
 
 export const useForgotEmailStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
 
-    const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
-
-
-    const ForgotEmailStyles  = {
-        display:'grid', 
-        fontSize: isExtraSmallScreen? XS_SCREEN_SMALL_FONT_SIZE: LG_SCREEN_SMALL_FONT_SIZE, 
-        color: '#007BFF'
-    }
-    return ForgotEmailStyles ;
+  const ForgotEmailStyles = {
+    display: "grid",
+    fontSize: isExtraSmallScreen
+      ? XS_SCREEN_SMALL_FONT_SIZE
+      : LG_SCREEN_SMALL_FONT_SIZE,
+    color: "#007BFF",
+  };
+  return ForgotEmailStyles;
 };
 
 export const useSendEmailStyles = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down(650));
 
-    const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down(300));
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down(650));
+  const SendEmailStyles = {
+    margin: "0 auto 0 auto",
+    height: "45px",
+    width: isSmallScreen ? "100%" : "60%",
+    backgroundColor: "#D9D9D9",
+    color: "#303030",
+    fontSize: isExtraSmallScreen
+      ? XS_SCREEN_MEDIUM_FONT_SIZE
+      : LG_SCREEN_MEDIUM_FONT_SIZE,
+    textTransform: "capitalize",
+    borderRadius: "5px",
 
-
-    const SendEmailStyles  = {
-        margin:'0 auto 0 auto', 
-        height: '45px',
-        width: isSmallScreen ? '100%' : '60%', 
-        backgroundColor:'#D9D9D9', 
-        color:'#303030', 
-        fontSize: isExtraSmallScreen ? XS_SCREEN_MEDIUM_FONT_SIZE : LG_SCREEN_MEDIUM_FONT_SIZE, 
-        textTransform: 'capitalize', 
-        borderRadius: '5px',   
-
-        '&:hover': {
-        backgroundColor: '#D9D9D9', 
-        color: '#303030', },
-    }
-    return SendEmailStyles;
+    "&:hover": {
+      backgroundColor: "#D9D9D9",
+      color: "#303030",
+    },
+  };
+  return SendEmailStyles;
 };
