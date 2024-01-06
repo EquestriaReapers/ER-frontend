cf9ddc0840d630ffd3d1d29a1439e0cf2cbce189
Feredev <jesusoortiz2410@gmail.com>
2024-01-06 17:39:21 -0400

    minor fixes

--- src/features/profile/profile-content/experiences/index.tsx
+++ src/features/profile/profile-content/experiences/index.tsx
@@ -8,22 +8,24 @@ const Experiences = () => {
   const { isEditable, profile } = useProfileContext()
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: { xs: '18px', sm: '45px' }
-      }}
-    >
-      <Box sx={boxTitleStyles}>
-        <Typography variant='h4' sx={experienceTitleStyles}>
-          Experiencia
-        </Typography>
-        {isEditable && <ExperienceModalWrapper />}
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: { xs: '18px', sm: '45px' },
+          width: { xs: '100%', sm: '100%', md: '70%', lg: '60%' }
+        }}
+      >
+        <Box sx={boxTitleStyles}>
+          <Typography variant='h4' sx={experienceTitleStyles}>
+            Experiencia
+          </Typography>
+          {isEditable && <ExperienceModalWrapper />}
+        </Box>
+        <ShowProfileExperiencies experiences={profile.experience} />
       </Box>
-
-      <ShowProfileExperiencies experiences={profile.experience} />
-    </Box>
+    </>
   )
 }
 
