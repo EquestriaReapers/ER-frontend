712e7dd95515f34f7b7c0611dfd799f5ae5441d6
Feredev <hector1567xd@gmail.com>
2024-01-06 14:23:22 -0400

    Fixes not fixes

--- src/features/catalogue/catalogue-content/components/DateRangePicker.tsx
+++ src/features/catalogue/catalogue-content/components/DateRangePicker.tsx
@@ -1,35 +1,33 @@
-import { Box } from '@mui/material'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
-import RemoveIcon from '@mui/icons-material/Remove'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { Box } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 const DateRangePicker = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '5px'
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "5px",
       }}
     >
       <Box
         sx={{
-          backgroundColor: 'white',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          borderRadius: '6px'
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          borderRadius: "6px",
         }}
       >
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
-            openTo='year'
-            views={['year']}
+            openTo="year"
+            views={["year"]}
             renderInput={(params) => <CalendarTodayIcon {...params} />}
           />
         </LocalizationProvider>
@@ -37,23 +35,23 @@ const DateRangePicker = () => {
       <RemoveIcon />
       <Box
         sx={{
-          backgroundColor: 'white',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          borderRadius: '6px'
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          borderRadius: "6px",
         }}
       >
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
-            openTo='year'
-            views={['year']}
+            openTo="year"
+            views={["year"]}
             renderInput={(params) => <CalendarTodayIcon {...params} />}
           />
         </LocalizationProvider>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default DateRangePicker
+export default DateRangePicker;
