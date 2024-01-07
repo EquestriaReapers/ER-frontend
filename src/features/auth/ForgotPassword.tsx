c0a68458eb2e0a303e32939375f293563c0942f3
jesus <jdrch62@gmail.com>
2024-01-07 18:08:52 -0400

    contraseña

--- src/features/auth/ForgotPassword.tsx
+++ src/features/auth/ForgotPassword.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import { FunctionComponent, useState, useCallback } from "react";
 import {
@@ -16,7 +15,6 @@ import {
   SearchBarBoxStyles,
   useEmailTypographyStyles,
   SearchBarTextFieldStyles,
-  useForgotEmailStyles,
   useSendEmailStyles,
 } from "./styles/ForgotPaswordStyles";
 import "../../styles/index.css";
@@ -24,14 +22,13 @@ import { ChangeEvent, FormEvent } from "react";
 import forgotPassword from "core/auth/forgot-password.service";
 import { useSuccessToast } from "hooks/use-success-toast";
 import { useErrorToast } from "hooks/use-error-toast";
-import forgotPasswordImage from "./images/forgot-password.jpg"
+import forgotPasswordImage from "./images/forgot-password.png"
 
 const ForgotPassword: FunctionComponent = () => {
   const CenterBoxStyles = useCenterBoxStyles();
   const RecoverPasswordTypographyStyles = useRecoverPasswordTypographyStyles();
   const RecoverPasswordTextStyles = useRecoverPasswordTextStyles();
   const EmailTypographyStyles = useEmailTypographyStyles();
-  const ForgotEmailStyles = useForgotEmailStyles();
   const SendEmailStyles = useSendEmailStyles();
 
   const [email, setEmail] = useState("");
@@ -48,7 +45,7 @@ const ForgotPassword: FunctionComponent = () => {
       event.preventDefault();
 
       try {
-        console.log(email);
+        if (!email) return;
         const data = await forgotPassword(email);
         setEmailSend(true);
         showSuccessToast(
@@ -93,7 +90,6 @@ const ForgotPassword: FunctionComponent = () => {
                     onChange={onEmailChange}
                     sx={SearchBarTextFieldStyles}
                   />
-                  <Link sx={ForgotEmailStyles}>¿Olvidó su correo?</Link>
                   </Box>
                   <Button sx={SendEmailStyles} type="submit">
                     Enviar Email
