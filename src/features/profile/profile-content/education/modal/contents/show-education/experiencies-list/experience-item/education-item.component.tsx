fe7b20b961e222d9d8ea99474b7cef34f1bca50c
Feredev <hector1567xd@gmail.com>
2024-01-07 23:41:54 -0400

    Si. porfavor. si. di,e que si porfavor.

--- src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
+++ src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
@@ -5,12 +5,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useContext } from "react";
 import { EducationContent } from "../../../../education-modal-context/types";
-import {
-  cvButtonStyle,
-  inlineStyles,
-  nameStyles,
-  subtitleStyles,
-} from "../styles";
+import { cvButtonStyle } from "../styles";
 import EducationModalContext from "../../../../education-modal-context/index";
 
 const EducationItem = ({ item, className }: Props) => {
@@ -23,45 +18,45 @@ const EducationItem = ({ item, className }: Props) => {
 
   return (
     <div className={className}>
-      <div>
-        <Box>
-          <Box className="titleIconStyles">
-            <Typography sx={nameStyles}>{item.title}</Typography>
-            <Box>
-              <IconButton>
-                <Typography sx={cvButtonStyle}>CV</Typography>
+      <Box>
+        <Box className="titleIconStyles">
+          <Box className={"typographyBoxStyles"}>
+            <Typography className={"nameStyles"}>{item.title}</Typography>
+          </Box>
+          <Box className={"itemIconBox"}>
+            <IconButton>
+              <Typography sx={cvButtonStyle}>CV</Typography>
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                setContent(EducationContent.Edit);
+                setAnEducation(item);
+              }}
+            >
+              <EditIcon sx={{ color: "#007935" }} />
+            </IconButton>
+            {item.isUCAB ? (
+              <IconButton disabled>
+                <DeleteIcon sx={{ color: "gray" }} />
               </IconButton>
+            ) : (
               <IconButton
                 onClick={() => {
-                  setContent(EducationContent.Edit);
+                  setContent(EducationContent.Delete);
                   setAnEducation(item);
                 }}
               >
-                <EditIcon sx={{ color: "#007935" }} />
+                <DeleteIcon sx={{ color: "#007935" }} />
               </IconButton>
-              {item.isUCAB ? (
-                <IconButton disabled>
-                  <DeleteIcon sx={{ color: "gray" }} />
-                </IconButton>
-              ) : (
-                <IconButton
-                  onClick={() => {
-                    setContent(EducationContent.Delete);
-                    setAnEducation(item);
-                  }}
-                >
-                  <DeleteIcon sx={{ color: "#007935" }} />
-                </IconButton>
-              )}
-            </Box>
-          </Box>
-          <Box sx={inlineStyles}>
-            <Typography sx={subtitleStyles} variant="h6">
-              {item.entity} {dateItem}
-            </Typography>
+            )}
           </Box>
         </Box>
-      </div>
+        <Box className={"inlineStyles"}>
+          <Typography className={"subtitleStyles"} variant="h6">
+            {item.entity} {dateItem}
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 };
