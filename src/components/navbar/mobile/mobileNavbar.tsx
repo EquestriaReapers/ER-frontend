1d849c53b5674521e76b6662c8fd9f410c7f23b0
GreenieWarren <87400557+GreenieWarren@users.noreply.github.com>
2024-01-07 23:45:52 -0400

    Preparando mobile para navbar

--- src/components/navbar/mobile/mobileNavbar.tsx
+++ src/components/navbar/mobile/mobileNavbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mui/material/";
+import { Box, Button, Typography } from "@mui/material/";
 import SvgComponent from "../SvgComponent";
 import { titleStyles, inlineStyles } from "../styles";
 import { Link, useNavigate } from "react-router-dom";
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,13 +1,16 @@
-import { Box, AppBar, Toolbar } from "@mui/material/";
+import { Box, AppBar, Toolbar, useMediaQuery } from "@mui/material/";
 import { navbarStyles } from "./styles";
 import Desktop from "./desktop";
+import Mobile from "./mobile";
 
 const Navbar = () => {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
   return (
     <Box>
       <AppBar position="static">
         <Toolbar sx={navbarStyles}>
-          <Desktop />
+          {isMobile ? <Mobile /> : <Desktop />}
         </Toolbar>
       </AppBar>
     </Box>
