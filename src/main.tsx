54224c2b5e9059f54b97a5e0aae3db85c1e37d7c
Feredev <hector1567xd@gmail.com>
2024-01-08 01:18:13 -0400

    Emergent commit to add NewPassword

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ import ForgotPassword from "features/auth/ForgotPassword";
 import Layout from "layout/Layout";
 import NotFound from "features/not-found/NotFound";
 import "styles/index.css";
+import NewPassword from "features/auth/NewPassword";
 
 const theme = createTheme({
   palette: {
