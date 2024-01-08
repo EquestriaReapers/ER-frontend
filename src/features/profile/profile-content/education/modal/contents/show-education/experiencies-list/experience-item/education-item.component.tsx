9f7983588176c6740bb5690a939c91c35ef9198b
GreenieWarren <87400557+GreenieWarren@users.noreply.github.com>
2024-01-07 20:06:12 -0400

    No hagan ramas desde staging
    Moví los cambios que hice en la rama response-styles hacia show-in-CV

--- src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
+++ src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
@@ -1,71 +1,60 @@
+import { Education } from "core/profiles/types";
 import Typography from "@mui/material/Typography";
 import { Box, IconButton } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import useUpdateEducationCV from "./use-update-CV-education";
 import { useContext } from "react";
 import { EducationContent } from "../../../../education-modal-context/types";
-import { nameStyles } from "../styles";
+import { cvButtonStyle } from "../styles";
 import EducationModalContext from "../../../../education-modal-context/index";
-import { Education } from "core/profiles/types";
 
 const EducationItem = ({ item, className }: Props) => {
   const { setContent, setAnEducation } = useContext(EducationModalContext);
   const getYear = (date: string | number | Date) =>
     new Date(date).getFullYear();
   const endYear = item.endDate ? getYear(item.endDate) : "No especificado";
+
   const dateItem = `(${endYear})`;
-  const updateEducationCV = useUpdateEducationCV(!item.isVisible, item);
 
   return (
     <div className={className}>
       <Box>
-        <Box>
-          <Box className="titleIconStyles">
-            <Typography sx={nameStyles}>{item.title}</Typography>
-            <Box>
-              <IconButton onClick={updateEducationCV} disabled={item.isUCAB}>
-                <Typography
-                  className={
-                    item.isUCAB
-                      ? "cvButtonStyleUCAB"
-                      : item.isVisible
-                      ? "cvButtonStyleTrue"
-                      : "cvButtonStyleFalse"
-                  }
-                >
-                  CV
-                </Typography>
+        <Box className="titleIconStyles">
+          <Box className={"typographyBoxStyles"}>
+            <Typography className={"nameStyles"}>{item.title}</Typography>
+          </Box>
+          <Box className={"itemIconBox"}>
+            <IconButton>
+              <Typography sx={cvButtonStyle}>CV</Typography>
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                setContent(EducationContent.Edit);
+                setAnEducation(item);
+              }}
+            >
+              <EditIcon sx={{ color: "#007935" }} />
+            </IconButton>
+            {item.isUCAB ? (
+              <IconButton disabled>
+                <DeleteIcon sx={{ color: "gray" }} />
               </IconButton>
+            ) : (
               <IconButton
                 onClick={() => {
-                  setContent(EducationContent.Edit);
+                  setContent(EducationContent.Delete);
                   setAnEducation(item);
                 }}
               >
-                <EditIcon sx={{ color: "#007935" }} />
+                <DeleteIcon sx={{ color: "#007935" }} />
               </IconButton>
-              {item.isUCAB ? (
-                <IconButton disabled>
-                  <DeleteIcon sx={{ color: "gray" }} />
-                </IconButton>
-              ) : (
-                <IconButton
-                  onClick={() => {
-                    setContent(EducationContent.Delete);
-                    setAnEducation(item);
-                  }}
-                >
-                  <DeleteIcon sx={{ color: "#007935" }} />
-                </IconButton>
-              )}
-            </Box>
+            )}
           </Box>
-          <Box className={"inlineStyles"}>
-            <Typography className={"subtitleStyles"} variant="h6">
-              {item.entity} {dateItem}
-            </Typography>
-          </Box>
+        </Box>
+        <Box className={"inlineStyles"}>
+          <Typography className={"subtitleStyles"} variant="h6">
+            {item.entity} {dateItem}
+          </Typography>
         </Box>
       </Box>
     </div>
--- src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.styled.tsx
+++ src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.styled.tsx
@@ -9,33 +9,6 @@ export default styled(EducationItem)`
     flex-wrap: wrap;
   }
 
-  .cvButtonStyleUCAB {
-    color: gray;
-    font-family: Inter;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-  }
-
-  .cvButtonStyleTrue {
-    color: #007934;
-    font-family: Inter;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-  }
-
-  .cvButtonStyleFalse {
-    color: #a6d6a8;
-    font-family: Inter;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-  }
-
   .typographyBoxStyles {
     display: flex;
     justify-content: space-between;
