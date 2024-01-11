2117f731bf9cc607f92eaa75b744a6e50252b8bd
Feredev <jesusoortiz2410@gmail.com>
2024-01-10 23:25:07 -0400

    primeros avances

--- src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
+++ src/features/profile/profile-content/education/modal/contents/show-education/experiencies-list/experience-item/education-item.component.tsx
@@ -1,37 +1,44 @@
-import { Education } from "core/profiles/types";
-import Typography from "@mui/material/Typography";
-import { Box, IconButton } from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useContext } from "react";
-import { EducationContent } from "../../../../education-modal-context/types";
-import EducationModalContext from "../../../../education-modal-context/index";
-import useUpdateEducationCV from "./use-update-CV-education";
+import { Education } from 'core/profiles/types'
+import Typography from '@mui/material/Typography'
+import { Box, IconButton } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { useContext } from 'react'
+import { EducationContent } from '../../../../education-modal-context/types'
+import EducationModalContext from '../../../../education-modal-context/index'
+import useUpdateEducationCV from './use-update-CV-education'
 
 const EducationItem = ({ item, className }: Props) => {
-  const { setContent, setAnEducation } = useContext(EducationModalContext);
-  const getYear = (date: string | number | Date) =>
-    new Date(date).getFullYear();
-  const endYear = item.endDate ? getYear(item.endDate) : "No especificado";
-  const dateItem = `(${endYear})`;
-  const updateEducationCV = useUpdateEducationCV(!item.isVisible, item);
+  const { setContent, setAnEducation } = useContext(EducationModalContext)
+  const getYear = (date: string | number | Date) => new Date(date).getFullYear()
+  const endYear = item.endDate ? getYear(item.endDate) : 'No especificado'
+  const dateItem = `(${endYear})`
+  const updateEducationCV = useUpdateEducationCV(!item.isVisible, item)
 
   return (
     <div className={className}>
       <Box>
-        <Box className="titleIconStyles">
-          <Box className={"typographyBoxStyles"}>
-            <Typography className={"nameStyles"}>{item.title}</Typography>
+        <Box className='titleIconStyles'>
+          <Box
+            className={'typographyBoxStyles'}
+            sx={{ gap: 2, alignItems: 'center' }}
+          >
+            <Typography className={'nameStyles'}>{item.title}</Typography>
+            {item.isUCAB && (
+              <Typography className={'principalButtonStyles'}>
+                {item.principal ? 'Principal' : 'Complementario'}
+              </Typography>
+            )}
           </Box>
-          <Box className={"itemIconBox"}>
+          <Box className={'itemIconBox'}>
             <IconButton onClick={updateEducationCV} disabled={item.isUCAB}>
               <Typography
                 className={
                   item.isUCAB
-                    ? "cvButtonStyleUCAB"
+                    ? 'cvButtonStyleUCAB'
                     : item.isVisible
-                    ? "cvButtonStyleTrue"
-                    : "cvButtonStyleFalse"
+                    ? 'cvButtonStyleTrue'
+                    : 'cvButtonStyleFalse'
                 }
               >
                 CV
@@ -39,41 +46,41 @@ const EducationItem = ({ item, className }: Props) => {
             </IconButton>
             <IconButton
               onClick={() => {
-                setContent(EducationContent.Edit);
-                setAnEducation(item);
+                setContent(EducationContent.Edit)
+                setAnEducation(item)
               }}
             >
-              <EditIcon sx={{ color: "#007935" }} />
+              <EditIcon sx={{ color: '#007935' }} />
             </IconButton>
             {item.isUCAB ? (
               <IconButton disabled>
-                <DeleteIcon sx={{ color: "gray" }} />
+                <DeleteIcon sx={{ color: 'gray' }} />
               </IconButton>
             ) : (
               <IconButton
                 onClick={() => {
-                  setContent(EducationContent.Delete);
-                  setAnEducation(item);
+                  setContent(EducationContent.Delete)
+                  setAnEducation(item)
                 }}
               >
-                <DeleteIcon sx={{ color: "#007935" }} />
+                <DeleteIcon sx={{ color: '#007935' }} />
               </IconButton>
             )}
           </Box>
         </Box>
-        <Box className={"inlineStyles"}>
-          <Typography className={"subtitleStyles"} variant="h6">
+        <Box className={'inlineStyles'}>
+          <Typography className={'subtitleStyles'} variant='h6'>
             {item.entity} {dateItem}
           </Typography>
         </Box>
       </Box>
     </div>
-  );
-};
-
-interface Props {
-  item: Education;
-  className?: string;
+  )
 }
 
-export default EducationItem;
+interface Props {
+  item: Education
+  className?: string
+}
+
+export default EducationItem
