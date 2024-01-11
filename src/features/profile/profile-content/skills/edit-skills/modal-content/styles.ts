5c05cde9c70b87d3fafc643f58052b2b6a5ac915
Feredev <hector1567xd@gmail.com>
2024-01-11 12:57:18 -0400

    Change Inter to inter

--- src/features/profile/profile-content/skills/edit-skills/modal-content/styles.ts
+++ src/features/profile/profile-content/skills/edit-skills/modal-content/styles.ts
@@ -20,7 +20,7 @@ export const modalStyle = {
 
 export const nameStyles = {
   fontSize: "20px",
-  fontFamily: "Inter",
+  fontFamily: "inter",
   lineHeight: "normal",
   fontStyle: "normal",
   fontWeight: "700",
@@ -30,7 +30,7 @@ export const descriptionStyles = {
   fontSize: "16px",
   fontWeight: "400",
   color: "#444444",
-  fontFamily: "Inter",
+  fontFamily: "inter",
   wordWrap: "break-word",
   pt: 2,
 };
