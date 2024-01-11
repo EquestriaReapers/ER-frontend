746c09317e572e1e8aaf13dc623f82e8e839b452
Feredev <hector1567xd@gmail.com>
2024-01-11 14:57:01 -0400

    Added more footer styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
-import Box from '@mui/material/Box'
-import Link from '@mui/material/Link'
-import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import TwitterIcon from '@mui/icons-material/Twitter'
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 import {
   FooterStyles,
@@ -11,44 +11,44 @@ import {
   useFooterFirstLineStyles,
   FooterTextStyles,
   FooterSecondLine,
-  FooterIconStyles
-} from './styles'
+  FooterIconStyles,
+} from "./styles";
 
 const Footer = () => {
-  const FooterFirstLineStyles = useFooterFirstLineStyles()
+  const FooterFirstLineStyles = useFooterFirstLineStyles();
 
   return (
     <Box sx={FooterStyles}>
       <Box sx={FooterLineStyles}></Box>
       <Box sx={InsideFooterStyles}>
         <Box sx={FooterFirstLineStyles}>
-          <Link href='/home' sx={FooterTextStyles}>
+          <Link href="/home" sx={FooterTextStyles}>
             Home
           </Link>
-          <Link href='/catalogue' sx={FooterTextStyles}>
+          <Link href="/catalogue" sx={FooterTextStyles}>
             Catálogo
           </Link>
-          <Link href='https://www.ucab.edu.ve/' sx={FooterTextStyles}>
+          <Link href="https://www.ucab.edu.ve/" sx={FooterTextStyles}>
             Página UCAB
           </Link>
-          <Link href='https://egresados.ucab.edu.ve/' sx={FooterTextStyles}>
+          <Link href="https://egresados.ucab.edu.ve/" sx={FooterTextStyles}>
             Página egresados
           </Link>
         </Box>
         <Box sx={FooterSecondLine}>
-          <Link href='https://www.instagram.com/enlaucab'>
+          <Link href="https://www.instagram.com/enlaucab">
             <InstagramIcon sx={FooterIconStyles} />
           </Link>
-          <Link href='https://www.facebook.com/enlaUCAB/'>
+          <Link href="https://www.facebook.com/enlaUCAB/">
             <FacebookRoundedIcon sx={FooterIconStyles} />
           </Link>
-          <Link href='https://twitter.com/enlaucab'>
+          <Link href="https://twitter.com/enlaucab">
             <TwitterIcon sx={FooterIconStyles} />
           </Link>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -2,65 +2,75 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { Theme } from "@mui/material";
 
 export const FooterStyles = {
-    width: "100%", 
-    backgroundColor: "#CDCDCD",   
-    height: "240px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "40px",
+  width: "100%",
+  backgroundColor: "#CDCDCD",
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+  minHeight: {
+    sm: "100px",
+    md: "inherit",
+  },
 };
 
-
 export const FooterLineStyles = {
-    width: "100%",
-    height: "3%",
-    backgroundColor: "#37B4E3"
-}
+  width: "100%",
+  height: "8px",
+  backgroundColor: "#37B4E3",
+};
 
 export const InsideFooterStyles = {
-    display: "flex",
-    width: "100%",
-    height: "40%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-}
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+};
 
 export const useFooterFirstLineStyles = () => {
+  const isSmallScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("sm")
+  );
 
-    const isLargeScreen = useMediaQuery((theme: Theme) =>
-        theme.breakpoints.down("lg")
-    );
-    const isSmallScreen = useMediaQuery((theme: Theme) =>
-        theme.breakpoints.down("sm")
-    );
+  const FooterFirstLineStyles = {
+    marginBottom: "30px",
+    width: isSmallScreen ? "270px" : "380px",
+    display: "flex",
+    flexDirection: isSmallScreen ? "column" : "row",
+    alignItems: "center",
+    gap: "10px",
+    justifyContent: "space-between",
+    fontSize: "16px",
+  };
 
-    const FooterFirstLineStyles = {
-        marginBottom: "30px",
-        width: isLargeScreen ? "270px" : "380px",
-        display: "flex",
-        flexDirection: isLargeScreen ? "column" : "row",
-        alignItems: isSmallScreen ? "center" : "center",
-        gap: isSmallScreen ? "0px" : "10px",
-        justifyContent: "space-between",
-        fontSize: isLargeScreen ? "12px" : "16px",
-    };
-
-    return FooterFirstLineStyles;
+  return FooterFirstLineStyles;
 };
 
 export const FooterTextStyles = {
-    color: "#343434",
-    textDecoration: "none",
-}
+  color: "#343434",
+  textDecoration: "none",
+  "&:hover": {
+    color: "#050505",
+  },
+};
 
 export const FooterSecondLine = {
-    width: "80px",
-    display: "flex",
-    justifyContent: "space-between",
-    gap: "20px",
-}
+  display: "flex",
+  justifyContent: "space-between",
+  flexWrap: "no-wrap",
+  gap: "20px",
+  mb: {
+    xs: 1,
+    sm: 2,
+    md: 3,
+    lg: 3,
+  },
+};
 
 export const FooterIconStyles = {
-    color: "#343434"
-}
+  color: "#343434",
+  "&:hover": {
+    color: "#050505",
+  },
+};
