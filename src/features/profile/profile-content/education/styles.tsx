b17dd6caece397a6b8c3d06307b30f19795d353a
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2024-01-12 13:48:02 -0400

    fuentes

--- src/features/profile/profile-content/education/styles.tsx
+++ src/features/profile/profile-content/education/styles.tsx
@@ -1,16 +1,16 @@
 export const educationTitleStyles = {
-  color: '#000',
-  fontFamily: 'inter',
-  fontSize: { xs: '22px', sm: '35px' },
-  fontStyle: 'normal',
-  fontWeight: '700',
-  lineHeight: 'normal'
-}
+  color: "#000",
+  fontFamily: "inter",
+  fontSize: { xs: "22px", sm: "30px" },
+  fontStyle: "normal",
+  fontWeight: "700",
+  lineHeight: "normal",
+};
 
 export const boxTitleStyles = {
-  display: 'flex',
-  flexWrap: 'nowrap',
-  flexDirection: 'row',
-  gap: '30px',
-  alignItems: 'center'
-}
+  display: "flex",
+  flexWrap: "nowrap",
+  flexDirection: "row",
+  gap: "30px",
+  alignItems: "center",
+};
