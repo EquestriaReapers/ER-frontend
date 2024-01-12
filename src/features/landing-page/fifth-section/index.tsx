4a5e1ba05dc8cdd2300075c0f11a795c0b666cbf
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2024-01-11 21:08:51 -0400

    estilos 5ta seccion

--- src/features/landing-page/fifth-section/index.tsx
+++ src/features/landing-page/fifth-section/index.tsx
@@ -1,86 +1,55 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { 
-    ThirdSectionStyles,
-    useInsideThirdSectionStyles,
-    InsideThirdSectionTextStyles,
-    InsideThirdSectionBoxTextStyles,
-    InsideThirdSectionTypographytStyles,
-    ThirdSectionTextStyles,
-    ThirdSectionTypographyStyles,
-    useThirdSectionFiguresSectionStyles,
-    useThirdSectionFirstFiguresSectionStyles,
-    useThirdSectionFirstFigureStyles,
-    ThirdSectionInsideFirstFigureStyles,
-    useThirdSectionFirstFigureTypographyStyles,
-    ThirdSectionFirstFigureTextStyles,
-    useThirdSectionSecondFiguresSectionStyles,
-    useThirdSectionSecondFigureStyles,
-    ThirdSectionInsideSecondFigureStyles,
-    useThirdSectionSecondFigureTypographyStyles,
-    ThirdSectionSecondFigureTextStyles,
+import {
+  ThirdSectionStyles,
+  useInsideThirdSectionStyles,
+  useThirdSectionFiguresSectionStyles,
+  useThirdSectionFirstFiguresSectionStyles,
+  useThirdSectionFirstFigureTypographyStyles,
+  ThirdSectionFirstFigureTextStyles,
+  useThirdSectionSecondFiguresSectionStyles,
+  useThirdSectionSecondFigureTypographyStyles,
+  ThirdSectionSecondFigureTextStyles,
 } from "./styles";
 
 const ThirdSection = () => {
+  const InsideThirdSectionStyles = useInsideThirdSectionStyles();
+  const ThirdSectionFiguresSectionStyles =
+    useThirdSectionFiguresSectionStyles();
+  const ThirdSectionFirstFiguresSectionStyles =
+    useThirdSectionFirstFiguresSectionStyles();
+  const ThirdSectionFirstFigureTypographyStyles =
+    useThirdSectionFirstFigureTypographyStyles();
+  const ThirdSectionSecondFiguresSectionStyles =
+    useThirdSectionSecondFiguresSectionStyles();
+  useThirdSectionSecondFigureTypographyStyles();
 
-    const InsideThirdSectionStyles = useInsideThirdSectionStyles();
-    const ThirdSectionFiguresSectionStyles = useThirdSectionFiguresSectionStyles();
-    const ThirdSectionFirstFiguresSectionStyles = useThirdSectionFirstFiguresSectionStyles();
-    const ThirdSectionFirstFigureStyles = useThirdSectionFirstFigureStyles();
-    const ThirdSectionFirstFigureTypographyStyles = useThirdSectionFirstFigureTypographyStyles();
-    const ThirdSectionSecondFiguresSectionStyles = useThirdSectionSecondFiguresSectionStyles();
-    const ThirdSectionSecondFigureStyles = useThirdSectionSecondFigureStyles();
-    const ThirdSectionSecondFigureTypographyStyles = useThirdSectionSecondFigureTypographyStyles();
-
-    return(
-        <Box sx={ThirdSectionStyles}>
-            <Box sx={InsideThirdSectionStyles}>
-                <Box sx={InsideThirdSectionTextStyles}>
-                    <Box>
-                        <Box sx={InsideThirdSectionBoxTextStyles}>
-                            <Typography sx={InsideThirdSectionTypographytStyles}>
-                                Lorem
-                            </Typography>
-                        </Box>
-                        <Box sx={ThirdSectionTextStyles}>
-                            <Typography sx={ThirdSectionTypographyStyles}>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Maxime mollitia, molestiae quas vel sint commodi
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box sx={ThirdSectionFiguresSectionStyles}>
-                    <Box sx={ThirdSectionFirstFiguresSectionStyles}>
-                        <Box sx={ThirdSectionFirstFigureStyles}>
-                            <Box sx={ThirdSectionInsideFirstFigureStyles}></Box>
-                        </Box>
-                        <Typography sx={ThirdSectionFirstFigureTypographyStyles}>
-                            Lorem
-                        </Typography>
-                        <Typography sx={ThirdSectionFirstFigureTextStyles}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Maxime mollitia, molestiae quas vel sint commodi
-                        </Typography>
-                    </Box>
-                    <Box sx={ThirdSectionSecondFiguresSectionStyles}>
-                        <Box sx={ThirdSectionSecondFigureStyles}>
-                            <Box sx={ThirdSectionInsideSecondFigureStyles}></Box>
-                        </Box>
-
-                        <Typography sx={ThirdSectionSecondFigureTypographyStyles}>
-                            Lorem
-                        </Typography>
-                        <Typography sx={ThirdSectionSecondFigureTextStyles}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Maxime mollitia, molestiae quas vel sint commodi
-                        </Typography>
-                    </Box>
-                </Box>
-            </Box>
+  return (
+    <Box sx={ThirdSectionStyles}>
+      <Box sx={InsideThirdSectionStyles}>
+        <Box sx={ThirdSectionFiguresSectionStyles}>
+          <Box sx={ThirdSectionFirstFiguresSectionStyles}>
+            <Typography sx={ThirdSectionFirstFigureTypographyStyles}>
+              ¿Eres un egresado?
+            </Typography>
+            <Typography sx={ThirdSectionFirstFigureTextStyles}>
+              Estamos comprometidos con brindar visibilidad e igualdad de
+              oportunidades a los profesionales de la UCAB
+            </Typography>
+          </Box>
+          <Box sx={ThirdSectionSecondFiguresSectionStyles}>
+            <Typography sx={ThirdSectionFirstFigureTypographyStyles}>
+              ¿Eres un reclutador o empresa?
+            </Typography>
+            <Typography sx={ThirdSectionSecondFigureTextStyles}>
+              ¡Contamos con una red completa de egresados profesionales que
+              cuenten con el sello de calidad ucabista!
+            </Typography>
+          </Box>
         </Box>
-    );
-
-}
+      </Box>
+    </Box>
+  );
+};
 
 export default ThirdSection;
