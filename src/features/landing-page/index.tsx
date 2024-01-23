3d3d6d6f4972c1b4de1a9485b9d45de64c7d80cc
pan <114714690+luizzzito@users.noreply.github.com>
2024-01-23 15:32:40 -0400

    Update index.tsx

--- src/features/landing-page/index.tsx
+++ src/features/landing-page/index.tsx
@@ -1,8 +1,4 @@
 import { Box } from "@mui/material";
-import {
-  useLandingPageContainerStyles,
-  useMainContainerStyles,
-} from "./styles";
 
 import FirstSection from "./first-section";
 import SecondSection from "./second-section";
@@ -11,9 +7,6 @@ import FourthSection from "./fourth-section";
 import FifthSection from "./fifth-section";
 
 const LandingPage = () => {
-  const LandingPageStyles = useLandingPageContainerStyles();
-  const MainContainerStyles = useMainContainerStyles();
-
   return (
     <Box>
       <FirstSection />
